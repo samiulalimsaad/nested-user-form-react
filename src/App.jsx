8d55b083df821ff4487f0abcdc40fe63f93891ef
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-20 12:31:06 +0600

    basic layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { initialState } from "./initialState";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <div className="grid place-items-center h-screen">
+            <div>
+                <div class="card bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        {Object.keys(initialState).map((type) => (
+                            <div id={type}>
+                                <fieldset className="">
+                                    <legend>{type}</legend>
+                                    <div className="flex gap-10 flex-wrap">
+                                        {Object.keys(initialState[type]).map(
+                                            (property) =>
+                                                Object.keys(
+                                                    initialState[type][property]
+                                                ).length ? (
+                                                    Object.keys(
+                                                        initialState[type][
+                                                            property
+                                                        ]
+                                                    ).map((v) => (
+                                                        <div class="form-control w-full max-w-xs">
+                                                            <label class="label">
+                                                                <span class="label-text">
+                                                                    {v}
+                                                                </span>
+                                                            </label>
+                                                            <input
+                                                                type="text"
+                                                                placeholder="Type here"
+                                                                name={v}
+                                                                className="input input-bordered input-primary "
+                                                            />
+                                                        </div>
+                                                    ))
+                                                ) : (
+                                                    <div class="form-control w-full max-w-xs">
+                                                        <label class="label">
+                                                            <span class="label-text">
+                                                                {property}
+                                                            </span>
+                                                        </label>
+                                                        <input
+                                                            type="text"
+                                                            placeholder="Type here"
+                                                            name={property}
+                                                            className="input input-bordered input-primary "
+                                                        />
+                                                    </div>
+                                                )
+                                        )}
+                                    </div>
+                                </fieldset>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
