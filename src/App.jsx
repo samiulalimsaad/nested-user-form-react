ff894c760a9392b95fd38740dbfe77914186d8e7
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-20 12:52:35 +0600

    basic layout updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <div className="grid place-items-center h-screen">
+        <div className="grid place-items-center h-screen p-20">
             <div>
                 <div class="card bg-base-100 shadow-xl">
-                    <div class="card-body">
+                    <div class="card-body space-y-4">
                         {Object.keys(initialState).map((type) => (
-                            <div id={type}>
-                                <fieldset className="">
-                                    <legend>{type}</legend>
+                            <div className="">
+                                <fieldset>
+                                    <legend className="font-semibold text-3xl capitalize">
+                                        {type}
+                                    </legend>
                                     <div className="flex gap-10 flex-wrap">
                                         {Object.keys(initialState[type]).map(
                                             (property) =>
@@ -24,9 +26,9 @@ function App() {
                                                             property
                                                         ]
                                                     ).map((v) => (
-                                                        <div class="form-control w-full max-w-xs">
-                                                            <label class="label">
-                                                                <span class="label-text">
+                                                        <div class="form-control w-full max-w-md">
+                                                            <label class="label capitalize">
+                                                                <span class="label-text text-xl">
                                                                     {v}
                                                                 </span>
                                                             </label>
@@ -39,9 +41,9 @@ function App() {
                                                         </div>
                                                     ))
                                                 ) : (
-                                                    <div class="form-control w-full max-w-xs">
-                                                        <label class="label">
-                                                            <span class="label-text">
+                                                    <div class="form-control w-full max-w-md">
+                                                        <label class="label capitalize">
+                                                            <span class="label-text text-xl">
                                                                 {property}
                                                             </span>
                                                         </label>
@@ -61,7 +63,7 @@ function App() {
                     </div>
                 </div>
                 <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <button class="btn btn-primary">submit</button>
                 </div>
             </div>
         </div>
