f9d8fa48437fa7cbad3db2f0e7e619cfa09e6cad
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-21 12:34:21 +0600

    initia table added

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,17 @@
 import { useState } from "react";
-import { initialState } from "./initialState";
+import Form from "./Components/Form";
+import Table from "./Components/Table";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [index, setIndex] = useState(0);
 
     return (
         <div className="grid place-items-center h-screen p-20">
-            <div>
-                <div class="card bg-base-100 shadow-xl">
-                    <div class="card-body space-y-4">
-                        {Object.keys(initialState).map((type) => (
-                            <div className="">
-                                <fieldset>
-                                    <legend className="font-semibold text-3xl capitalize">
-                                        {type}
-                                    </legend>
-                                    <div className="flex gap-10 flex-wrap">
-                                        {Object.keys(initialState[type]).map(
-                                            (property) =>
-                                                Object.keys(
-                                                    initialState[type][property]
-                                                ).length ? (
-                                                    Object.keys(
-                                                        initialState[type][
-                                                            property
-                                                        ]
-                                                    ).map((v) => (
-                                                        <div class="form-control w-full max-w-md">
-                                                            <label class="label capitalize">
-                                                                <span class="label-text text-xl">
-                                                                    {v}
-                                                                </span>
-                                                            </label>
-                                                            <input
-                                                                type="text"
-                                                                placeholder="Type here"
-                                                                name={v}
-                                                                className="input input-bordered input-primary "
-                                                            />
-                                                        </div>
-                                                    ))
-                                                ) : (
-                                                    <div class="form-control w-full max-w-md">
-                                                        <label class="label capitalize">
-                                                            <span class="label-text text-xl">
-                                                                {property}
-                                                            </span>
-                                                        </label>
-                                                        <input
-                                                            type="text"
-                                                            placeholder="Type here"
-                                                            name={property}
-                                                            className="input input-bordered input-primary "
-                                                        />
-                                                    </div>
-                                                )
-                                        )}
-                                    </div>
-                                </fieldset>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">submit</button>
-                </div>
+            <div className="space-x-4">
+                <button className="text-white btn btn-accent">Form</button>
+                <button className="text-white btn btn-warning">Table</button>
             </div>
+            {index ? <Form /> : <Table />}
         </div>
     );
 }
