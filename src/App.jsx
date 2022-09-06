e52458a7d5ed913048fe4e78c562cbb1349b5a12
Samiul Alim <57401611+samiulalimsaad@users.noreply.github.com>
2022-09-06 18:10:59 +0600

    Revert "`dependencies` added"

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import { useState } from "react";
-import Table from "./Components/Table";
-import UserForm from "./Components/UserForm";
-import { dummyState } from "./state";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
-    const [index, setIndex] = useState(0);
+  const [count, setCount] = useState(0)
 
-    const [state, setState] = useState(dummyState);
-
-    return (
-        <div className="grid place-items-center h-screen p-20">
-            <div className="space-x-4">
-                <button
-                    onClick={() => setIndex(1)}
-                    className="text-white btn btn-accent"
-                >
-                    Form
-                </button>
-                <button
-                    onClick={() => setIndex(0)}
-                    className="text-white btn btn-warning"
-                >
-                    Table
-                </button>
-            </div>
-            {index ? <UserForm setState={setState} /> : <Table state={state} />}
-        </div>
-    );
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
 
-export default App;
+export default App
